ea80ac71c3e09551088b3445c8ce1f9e22b6d86f
MoXcz <130593875+MoXcz@users.noreply.github.com>
2026-07-01 09:40:09 -0600

    wip; test this in different machines

--- omarchy/.config/waybar/scripts/cpu.sh
+++ omarchy/.config/waybar/scripts/cpu.sh
@@ -2,6 +2,7 @@
 
 CPU_USAGE=$((100-$(vmstat 1 2 | tail -1 | awk '{print $15}')))
 CPU_TEMP=$(awk '{printf "%.0f", $1/1000}' /sys/class/hwmon/hwmon1/temp1_input)
+# CPU_TEMP=$(sensors | awk '/Package id 0:/ {gsub("\\+","",$4); gsub("°C","",$4); print int($4)}')
 
 [ -z "$CPU_USAGE" ] && CPU_USAGE="N/A"
 [ -z "$CPU_TEMP" ] && CPU_TEMP="N/A"
