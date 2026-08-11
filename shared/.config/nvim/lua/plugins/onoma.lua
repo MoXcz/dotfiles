771ad7525b299abbffea1416797c4c9bec028127
MoXcz <130593875+MoXcz@users.noreply.github.com>
2026-08-10 19:56:01 -0600

    feat(nvim): add onoma installation instructions on comment

--- shared/.config/nvim/lua/plugins/onoma.lua
+++ shared/.config/nvim/lua/plugins/onoma.lua
@@ -4,4 +4,5 @@ require("onoma").setup({
   picker = { 'snacks' },
 })
 
+-- cd ~/.local/share/nvim/site/pack/core/opt/onoma.nvim && cargo --config ./bridge/.cargo/config.toml build --release --manifest-path ./bridge/Cargo.toml
 vim.keymap.set({ 'n', 'v', 'x' }, '<leader>fa', Snacks.picker.get_symbols, { desc = 'Symbols', silent = true })
