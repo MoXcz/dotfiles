22f85fad2b0b6db2205850b459c8d6fc14a5dcd0
MoXcz <130593875+MoXcz@users.noreply.github.com>
2026-09-18 17:59:45 -0600

    fix: cpu scriptf failed if sensor name did not match

--- shared/.config/waybar/scripts/cpu.sh
+++ shared/.config/waybar/scripts/cpu.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
-CPU_USAGE=$((100-$(vmstat 1 2 | tail -1 | awk '{print $15}')))
-CPU_TEMP=$(awk '{printf "%.0f", $1/1000}' /sys/class/hwmon/hwmon1/temp1_input)
-# CPU_TEMP=$(sensors | awk '/Package id 0:/ {gsub("\\+","",$4); gsub("°C","",$4); print int($4)}')
+CPU_USAGE=$((100 - $(vmstat 1 2 | tail -1 | awk '{print $15}')))
+
+# find CPU sensor by driver name: coretemp (Intel), k10temp / zenpower (AMD).
+CPU_TEMP=""
+for dir in /sys/class/hwmon/hwmon*; do
+  case "$(cat "$dir/name" 2>/dev/null)" in
+  coretemp | k10temp | zenpower)
+    [[ -r $dir/temp1_input ]] && CPU_TEMP=$(awk '{printf "%.0f", $1/1000}' "$dir/temp1_input")
+    break
+    ;;
+  esac
+done
 
 [ -z "$CPU_USAGE" ] && CPU_USAGE="N/A"
 [ -z "$CPU_TEMP" ] && CPU_TEMP="N/A"
