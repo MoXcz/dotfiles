df076ac5678da820ae12165602462c48a30d6403
MoXcz <130593875+MoXcz@users.noreply.github.com>
2026-09-18 18:08:37 -0600

    fix: localsend rule didn't work due to different app name

--- hermit/.config/hypr/apps/localsend.lua
+++ hermit/.config/hypr/apps/localsend.lua
@@ -1,3 +1,2 @@
--- Float LocalSend and fzf file picker.
-o.window("(Share|localsend)", { float = true, center = true })
-o.window("localsend", { size = { 1100, 700 } })
+o.window("org.localsend.localsend_app", { float = true, center = true, size = { 1100, 700 } })
+o.window("Share", { tag = "+floating-window" })
